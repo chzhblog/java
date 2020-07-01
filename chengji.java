82586dc3044ac568747587ee9d654be429c42e85
mrchzh <cz9010@sina.cn>
2020-07-01 11:10:07 +0800

    2020.7.1

---
+++ chengji.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class chengji {
+    public static void main(String[]args){
+        System.out.print("请输入学生成绩:");
+        Scanner input = new Scanner(System.in);
+        int naber = input.nextInt();
+        input.close();
+        if(naber>=0 && naber <=60 ){
+            System.out.print("不及格");
+        }else if( naber >=60 && naber <=80){
+            System.out.print("良好");
+        }else if(naber >=80 && naber <=100){
+            System.out.print("优秀");
+        }else{
+            System.out.print("你输入的成绩不合法");
+
+        }
+
+    }
+    
+}
