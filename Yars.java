11b710303093b231f9af23441ba03c905d258556
mrchzh <cz9010@sina.cn>
2020-07-01 14:52:38 +0800

    判断是否闰年

---
+++ Yars.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+/**
+ * 输入一个年份，判断是否为闰年/平年
+ */
+
+public class Yars{
+    public static void  main(String[]args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("请输入年份:");
+  
+        int yars = in.nextInt();
+        if (yars%4 == 0 && yars%100 !=0) {
+            System.out.println(yars+"年是闰年");
+        }else{
+            System.out.println(yars+"年是平年");
+        }
+        in.close();
+    }
+    
+
+}
