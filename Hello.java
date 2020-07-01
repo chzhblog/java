e7c86c55b14d5a295aa9cb0cff1743fe6d535fbc
mrchzh <cz9010@sina.cn>
2020-07-01 10:21:28 +0800

    2020.7.1test

--- Hello.java
+++ Hello.java
@@ -5,7 +5,7 @@ public class Hello {
 	public static void main(String[] args) {
 		 System.out.println("你好，java！");
 		 System.out.println("HELLO java");
-		 System.out.println("111111111111111111111111111");
+		 System.out.println("This is my a java test code!!");
 		
 	}
 }
